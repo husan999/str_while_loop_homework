016f00c30dbf9611503d3366a7329109b0eb3458
husan999 <husanshunqorov07@gmail.com>
2023-03-17 22:58:42 +0500

    While03

--- While03.py
+++ While03.py
@@ -6,4 +6,14 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    a=0
+    i=0
+    while i<len(s):
+
+        if s[i].isdigit() or s[i].isalpha():
+            a+=0
+        else:
+            a+=1
+        i+=1
+    return a
+print(main("g''j's"))
