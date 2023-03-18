2a0386f44ca060c6ea37bb61fa7f71daa30892d3
husan999 <husanshunqorov07@gmail.com>
2023-03-18 07:07:22 +0500

    While09

--- While09.py
+++ While09.py
@@ -6,4 +6,11 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    a=0
+    i=0
+    while i<len(s):
+        if str(s[i]).isdigit():
+                a+=int(s[i])
+        i+=1
+    return a
+print(main("987654"))
